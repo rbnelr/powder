b52cb2efbe2ef6575be9e5ddc293c56cff1a603f
Anon Ymous <rbnelr@gmx.de>
2019-10-19 02:33:15 +0200

    inital commit

---
+++ Assets/MouseTool.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Unity.Mathematics.math;
+using Unity.Mathematics;
+
+public class MouseTool : MonoBehaviour {
+	public PowderSim Sim;
+
+	MeshRenderer MeshRenderer;
+	private void Start () {
+		MeshRenderer = GetComponent<MeshRenderer>();
+	}
+
+	public float MouseScrollDeltaSens = 0.5f;
+
+
+	public PowderSim.Cell.Type DrawType = PowderSim.Cell.Type.WOOD;
+	public float DrawSize = 5f;
+
+	void Update () {
+		var plane = new Plane(new Vector3(0, 0, -1), 0);
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		bool mouseHit = plane.Raycast(ray, out float dist);
+
+		MeshRenderer.enabled = mouseHit;
+		if (mouseHit) {
+			float3 pos = ray.origin + ray.direction * dist;
+			transform.position = float3(pos.xy, transform.position.z);
+		}
+
+		{ // Update Tool Size
+			float size_log = log2(DrawSize);
+			size_log += Input.mouseScrollDelta.y * MouseScrollDeltaSens;
+			DrawSize = clamp(pow(2f, size_log), 1, 1024); 
+
+			float Diameter = DrawSize * Sim.transform.localScale.y / Sim.Resolution.y; // size in world space of mouse cursor
+
+			transform.localScale = float3(Diameter);
+			MeshRenderer.material.SetFloat("_Thickness", 0.33f*2 / DrawSize);
+		}
+
+		if (Input.GetMouseButton(0)) { // Draw
+			Draw(DrawType);
+		} else if (Input.GetMouseButton(1)) { // Erase
+			Draw(PowderSim.Cell.Type.AIR);
+		} else {
+			draw_prev_frame = false;
+		}
+	}
+	
+	bool draw_prev_frame = false;
+	float2 prev_center;
+	float  prev_radius;
+
+	void Draw (PowderSim.Cell.Type Type) {
+		float2 cur_center = ((float3)Sim.transform.InverseTransformPoint(transform.position)).xy + 0.5f;
+		float  cur_radius = ((float3)Sim.transform.InverseTransformVector(transform.localScale)).y / 2;
+
+		cur_center *= Sim.Resolution;
+		cur_radius *= Sim.Resolution.y;
+		
+		// since moving the mouse fast results in in skipping pixels between frames
+		// we store the prev frames mouse position and step through all pixels on the line from prev frame and cur frame pos to never skip pixels
+		float dist = draw_prev_frame ? distance(cur_center, prev_center) : 0f;
+		float cur_dist = 0;
+
+		do {
+			float t = dist != 0f ? cur_dist / dist : cur_dist;
+
+			float2 center = lerp(cur_center, prev_center, t);
+			float  radius = lerp(cur_radius, prev_radius, t);
+
+			int2 a = clamp( (int2)floor(center - radius), 0, Sim.Resolution);
+			int2 b = clamp( (int2)ceil (center + radius), 0, Sim.Resolution);
+
+			for (int y=a.y; y<b.y; ++y) {
+				for (int x=a.x; x<b.x; ++x) {
+					if (distance(float2(x,y) + 0.5f, center) <= radius + 0.2f)
+						Sim.Cells[y,x] = new PowderSim.Cell { type = Type };
+				}
+			}
+
+			cur_dist += 1;
+		} while (cur_dist < dist);
+
+		prev_center = cur_center;
+		prev_radius = cur_radius;
+		
+		draw_prev_frame = true;
+	}
+}
---
+++ Assets/PowderSim.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Unity.Mathematics.math;
+using Unity.Mathematics;
+
+public class PowderSim : MonoBehaviour {
+	public struct Cell {
+		public enum Type {
+			AIR,
+			WOOD,
+		}
+
+		public Type type;
+
+		public override string ToString () => type.ToString();
+	}
+	
+	public int2 Resolution = int2(300, 200);
+
+	public Cell[,] Cells;
+	Color32[] Pixels;
+
+	Texture2D texture;
+
+	public Color32[] CellTypeColors;
+
+	public bool Reset = true;
+
+	public GameObject Background;
+
+	void Update () {
+		Resolution = max(Resolution, 1);
+
+		if (Cells == null || Cells.GetLength(1) != Resolution.x || Cells.GetLength(0) != Resolution.y) {
+			Cells = new Cell[Resolution.y, Resolution.x];
+			Pixels = new Color32[Resolution.y * Resolution.x];
+
+			texture = new Texture2D(Resolution.x, Resolution.y, TextureFormat.RGBA32, false, false);
+			texture.filterMode = FilterMode.Point;
+			
+			GetComponent<MeshRenderer>().material.SetTexture("_MainTex", texture);
+		}
+
+		// scale texture plane to make pixels non-stretched
+		float aspect = (float)Resolution.x / (float)Resolution.y;
+
+		var scale = this.transform.localScale;
+		scale.x = scale.y * aspect;
+		this.transform.localScale = scale;
+
+		// fix backround texture stretching
+		float background_aspect = 16f / 9f;
+		Background.GetComponent<MeshRenderer>().material.SetVector("_BaseMap_ST", float4(aspect / background_aspect, 1, 0.5f - aspect / background_aspect / 2, 0));
+
+		if (Reset) {
+			for (int y=0; y<Resolution.y; ++y) {
+				for (int x=0; x<Resolution.x; ++x) {
+					Cells[y,x] = new Cell { type = Cell.Type.AIR };
+				}
+			}
+
+			Reset = false;
+		}
+		
+		for (int y=0; y<Resolution.y; ++y) {
+			for (int x=0; x<Resolution.x; ++x) {
+				Pixels[y * Resolution.x + x] = CellTypeColors[ clamp((int)Cells[y,x].type, 0, CellTypeColors.Length -1) ];
+			//	Pixels[y * Resolution.x + x] = ((x % 2) ^ (y % 2)) == 0 ? new Color32(255, 0, 0, 255) : new Color32(0, 0, 255, 0);
+			}
+		}
+		
+		texture.SetPixels32(Pixels, 0);
+		texture.Apply();
+	}
+}
